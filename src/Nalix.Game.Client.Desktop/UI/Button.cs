96087451a730105dc904d423288290ecdce57185
phucnguyen <nguyen098xx@gmail.com>
2025-05-05 22:59:29 +0700

    0.0.8

--- src/Nalix.Game.Client.Desktop/UI/Button.cs
+++ src/Nalix.Game.Client.Desktop/UI/Button.cs
@@ -1,12 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SFML.Graphics;
+using SFML.System;
+using System;
 
-namespace Nalix.Game.Client.Desktop.UI
+namespace Nalix.Game.Client.Desktop.UI;
+
+internal class Button
 {
-    internal class Button
+    private readonly RectangleShape _shape;
+    private readonly Text _text;
+
+    public FloatRect Bounds => _shape.GetGlobalBounds();
+    public Action OnClick { get; set; }
+
+    public Button(string label, Font font, Vector2f position, Vector2f size)
     {
+        _shape = new RectangleShape(size)
+        {
+            Position = position,
+            FillColor = new Color(70, 70, 70),
+            OutlineColor = Color.White,
+            OutlineThickness = 2
+        };
+
+        FloatRect bounds = _text.GetLocalBounds();
+
+        _text = new Text(label, font, 24)
+        {
+            FillColor = Color.White,
+            Position = new Vector2f(
+                position.X + (size.X - bounds.Width) / 2 - bounds.Left,
+                position.Y + (size.Y - bounds.Height) / 2 - bounds.Top
+            )
+        };
+    }
+
+    public void Draw(RenderWindow window)
+    {
+        window.Draw(_shape);
+        window.Draw(_text);
+    }
+
+    public void HandleMouseClick(Vector2i mousePosition)
+    {
+        if (Bounds.Contains(mousePosition.X, mousePosition.Y))
+            OnClick?.Invoke();
+    }
+
+    public void HandleMouseHover(Vector2i mousePosition)
+    {
+        if (Bounds.Contains(mousePosition.X, mousePosition.Y))
+            _shape.FillColor = new Color(90, 90, 90); // hover
+        else
+            _shape.FillColor = new Color(70, 70, 70); // default
     }
 }
--- src/Nalix.Game.Client.Desktop/UI/Label.cs
+++ src/Nalix.Game.Client.Desktop/UI/Label.cs
@@ -1,12 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SFML.Graphics;
+using SFML.System;
 
-namespace Nalix.Game.Client.Desktop.UI
+namespace Nalix.Game.Client.Desktop.UI;
+
+internal class Label
 {
-    internal class Label
+    private readonly Text _text;
+
+    public Label(string content, Font font, uint size, Vector2f position, Color? color = null)
     {
+        _text = new Text(content, font, size)
+        {
+            FillColor = color ?? Color.White
+        };
+
+        // Căn giữa text so với vị trí
+        FloatRect bounds = _text.GetLocalBounds();
+        _text.Origin = new Vector2f(bounds.Left + bounds.Width / 2f, bounds.Top + bounds.Height / 2f);
+        _text.Position = position;
+    }
+
+    public void SetText(string content)
+    {
+        _text.DisplayedString = content;
+
+        // Cập nhật lại Origin sau khi đổi nội dung
+        FloatRect bounds = _text.GetLocalBounds();
+        _text.Origin = new Vector2f(bounds.Left + bounds.Width / 2f, bounds.Top + bounds.Height / 2f);
+    }
+
+    public void Draw(RenderWindow window)
+    {
+        window.Draw(_text);
+    }
+
+    public Vector2f Position
+    {
+        get => _text.Position;
+        set => _text.Position = value;
+    }
+
+    public string Text
+    {
+        get => _text.DisplayedString;
+        set => SetText(value);
+    }
+
+    public Color FillColor
+    {
+        get => _text.FillColor;
+        set => _text.FillColor = value;
     }
 }
