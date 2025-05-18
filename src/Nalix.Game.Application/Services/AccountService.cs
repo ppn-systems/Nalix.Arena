e9037fbfe15779b11d92e635f9f43f291e8f652d
phucnguyen <nguyen098xx@gmail.com>
2025-05-18 11:40:38 +0700

    0.3.0

---
+++ src/Nalix.Game.Application/Services/AccountService.cs
@@ -0,0 +1,17 @@
+﻿using Nalix.Common.Connection;
+using Nalix.Common.Package;
+using Nalix.Common.Package.Attributes;
+using Nalix.Common.Security;
+using System.Threading.Tasks;
+
+namespace Nalix.Game.Application.Services;
+
+[PacketController]
+internal class AccountService
+{
+    [PacketId((ushort)Command.Login)]
+    [PacketPermission(PermissionLevel.Guest)]
+    internal async Task LoginAsync(IPacket packet, IConnection connection)
+    {
+    }
+}
---
+++ src/Nalix.Game/Command.cs
@@ -0,0 +1,9 @@
+﻿namespace Nalix.Game;
+
+public enum Command : ushort
+{
+    /// <summary>
+    /// Lệnh để người chơi đăng nhập vào trò chơi.
+    /// </summary>
+    Login = 0x64,
+}
